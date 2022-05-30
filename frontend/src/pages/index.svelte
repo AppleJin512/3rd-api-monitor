c62706a2bcb8993c0da38fe2e685428c9029c08e
AppleJin512 <christianjin512@gmail.com>
2022-05-30 20:43:15 +0800

    feat: 🎸 change index page in frontend

--- frontend/src/pages/index.svelte
+++ frontend/src/pages/index.svelte
@@ -1,2 +1,37 @@
 <script lang="ts">
+  import axios from "axios";
+  import { baseURL } from "../services/Constants";
+  import { onMount } from "svelte";
+  let endpointList = [];
+  onMount(() => {
+    endpointList = [];
+    axios
+      .get(baseURL)
+      .then((res) => {
+        if (res.data) {
+          endpointList = res.data;
+        }
+      })
+      .catch((err) => console.log(err));
+  });
 </script>
+
+<div class="w-2/3 mx-auto py-10">
+  {#each endpointList as endpoint}
+    <h2 class="font-bold my-2 text-xl">{endpoint.name}</h2>
+    <ul>
+      {#each endpoint.urls as item}
+        <ol class="flex items-center mb-2">
+          <img
+            src="/images/icon-{item.health ? 'green' : 'red'}.svg"
+            alt="health"
+            class="w-6 h-6 mr-4"
+          />
+          <a href={item.url} class="underline cursor-pointer" target="_blank"
+            >{item.url}</a
+          >
+        </ol>
+      {/each}
+    </ul>
+  {/each}
+</div>
---
+++ frontend/src/services/Constants.ts
@@ -0,0 +1,1 @@
+export const baseURL = "https://doobtvjcpb8dc.cloudfront.net/health";
