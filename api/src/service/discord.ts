3a22fa830d0c6ddc04b4f3488648bfd822c4bbf5
AppleJin512 <christianjin512@gmail.com>
2022-06-01 09:35:42 +0800

    perf: ⚡️ add discord webhook schema (#2)

--- api/src/service/discord.ts
+++ api/src/service/discord.ts
@@ -1,12 +1,46 @@
 import axios from 'axios';
+import {URL} from 'url';
 import {stringTable} from '../constant';
+
+export interface DiscordWebhookEmbbed {
+  title?: string;
+  color?: number;
+  description?: string;
+  timestamp?: string;
+  url?: string;
+  icon_url?: URL;
+  image?: {url: URL};
+  thumbnail?: {url: URL};
+  fields?: Array<{
+    name?: string;
+    value?: string;
+    inline?: boolean;
+  }>;
+}
+
+export interface DiscordWebhookBody {
+  content?: string;
+  username?: string;
+  avatar_url?: URL;
+  embeds?: Array<DiscordWebhookEmbbed>;
+}
+
+/**
+ * @param color: html color. E.g: #dc143c
+ * @return number: int value. E.g: 14423100
+ */
+export function htmlColorToNumber(color: string) {
+  return parseInt(color.slice(1), 16);
+}
 
 function formatForDiscord(title: string, body: string) {
   return `${title}\n\`\`\`${body}\`\`\``;
 }
 
-function send(content: string, targetChannel: string) {
-  axios.post(targetChannel, {content});
+async function send(body: DiscordWebhookBody, targetChannel: string) {
+  axios.post(targetChannel, body, {
+    headers: {'Content-Type': 'application/json'},
+  });
 }
 
 export function sendStringToDiscord(
@@ -14,7 +48,7 @@ export function sendStringToDiscord(
   body: string,
   targetChannel: string
 ) {
-  send(formatForDiscord(title, body), targetChannel);
+  send({content: formatForDiscord(title, body)}, targetChannel);
 }
 
 export function sendObjToDiscord(
